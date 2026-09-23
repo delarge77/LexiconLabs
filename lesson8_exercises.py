91687d0fb6c035dfcb03ea9d3d95c59902003265
delarge77 <lastripper@hotmail.com>
2026-09-23 12:54:54 +0200

    more exercises

--- lesson8_exercises.py
+++ lesson8_exercises.py
@@ -52,9 +52,70 @@
 #                 return "Average Movie"
 
 # C
+# class Account:
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.balance = balance
+
+# class SavingsAccount(Account):
+#     def __init__(self, owner, balance, interest_rate):
+#         super().__init__(owner, balance)
+#         self.interest_rate = interest_rate
+
+# account = Account("Alessandro", 60000)
+# savings = SavingsAccount(account.owner, account.balance, 0.50)
+
+# print(account.owner, account.balance)
+# print(savings.owner, savings.balance, savings.interest_rate)
+
+# D
+# class Employee:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def get_information(self):
+#         return f"Employee {self.name}"
+
+# class Developer(Employee):
+#     def greetingDeveloper(self):
+#         return f"{self.name} is a developer"
+
+# class Teacher(Employee):
+#     def greetingTeacher(self):
+#             return f"{self.name} is a teacher"
+    
+# developer = Developer("Alessandro")
+# teacher = Teacher("Maria")
+
+# print(developer.greetingDeveloper())
+# print(teacher.greetingTeacher())
+
+# E
+# class Device:
+#     def __init__(self, brand, year):
+#         self.brand = brand
+#         self.year = year
+#         self.is_active = True
+
+#         if self.year < 1990:
+#             self.is_active = False
+
+    
+
+# class Laptop(Device):
+#     def __init__(self, brand, year, ram_gb):
+#         super().__init__(brand, year)
+#         self.ram_gb = ram_gb
+
+# class Mobile(Device):
+#     def __init__(self, brand, year, model):
+#         super().__init__(brand, year)
+#         self.model = model
+
+# laptop = Laptop("Apple", 1989, 256)
+# mobile = Mobile("Apple", 2026, 256)
 
 
-
-
-
+# print(laptop.brand, laptop.year, laptop.ram_gb, laptop.is_active)
+# print(mobile.brand, mobile.year, mobile.model, mobile.is_active)
 
