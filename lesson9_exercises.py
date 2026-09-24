f6dec70c6a347de1b10346f00086062690ff12bb
delarge77 <lastripper@hotmail.com>
2026-09-24 10:50:37 +0200

    rename file and more exercises

---
+++ lesson9_exercises.py
@@ -0,0 +1,49 @@
+# A
+# class EmailNotification:
+#     def send(self):
+#         return "This is an EMAIL notification"
+
+# class SMSNotification:
+#     def send(self):
+#             return "This is an SMS notification"
+
+# class PushNotification:
+#     def send(self):
+#             return "This is an PUSH notification"
+
+
+# notifications = [EmailNotification(), SMSNotification(), PushNotification()]
+
+# for notification in notifications:
+#      print(notification.send())
+
+# The loop doesnt need to know which exact objct is since all of them implement same method
+
+# B
+# class Document:
+#     def __init__(self, title) -> None:
+#         self.title = title
+
+#     def describe(self):
+#         return self.title
+
+# class PDFDocument(Document):
+#     def describe(self):
+#         return "This is a PDF Document"
+
+# class TextDocument(Document):
+#     def describe(self):
+#             return "This is a TEXT Document"
+
+# pdf1 = PDFDocument("pdf1")
+# pdf2 = PDFDocument("pdf2")
+# txt1 = TextDocument("txt1")
+# txt2 = TextDocument("txt2")
+
+# files = [pdf1, pdf2, txt1, txt2]
+
+# for file in files:
+#      print(file.title)
+#      print(file.describe())
+
+
